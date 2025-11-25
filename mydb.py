7b2f07f78e55a08d1ecc87d7e28e0919ec85a4dc
Lotfy Mohamed Lotfy <81247262+lotfy1654@users.noreply.github.com>
2025-11-25 23:43:40 +0200

    Start Project

---
+++ mydb.py
@@ -0,0 +1,23 @@
+# Install Mysql on my computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql 
+# pip install mysql-connector-python
+# pip install mysql-connector
+
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host ="localhost",
+    user ="root",
+    passwd ="password123"
+)
+
+
+# prepare a cursor object
+cursor_object = dataBase.cursor()
+
+# Create a database
+cursor_object.execute("CREATE DATABASE crm_db")
+
+print("Database created successfully!")
