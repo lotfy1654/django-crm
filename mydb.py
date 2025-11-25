49704fc10d45ce6dd4488e64c5aaddd2087bab25
Lotfy Mohamed Lotfy <81247262+lotfy1654@users.noreply.github.com>
2025-11-26 00:12:49 +0200

    Connect to MySQL

--- mydb.py
+++ mydb.py
@@ -8,11 +8,11 @@
 import mysql.connector
 
 dataBase = mysql.connector.connect(
-    host ="localhost",
-    user ="root",
-    passwd ="password123"
+    host="localhost",
+    user="root",
+    passwd="password123",
+    auth_plugin='mysql_native_password'
 )
-
 
 # prepare a cursor object
 cursor_object = dataBase.cursor()
