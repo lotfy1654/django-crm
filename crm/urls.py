734412efdd6745fc8769adab43af5cc9f4d930fa
Lotfy Mohamed Lotfy <81247262+lotfy1654@users.noreply.github.com>
2025-11-26 03:12:47 +0200

    Start Register

--- crm/urls.py
+++ crm/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login_user, name='loginurl'),
     path('logout', views.logout_user, name='logouturl'),
+    path('register', views.register_user, name='registerurl'),
     path('about', views.about, name='abouturl'),
 ]
